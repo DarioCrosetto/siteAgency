2ad4f54fcc448063260d8acf89f34556179e7e75
Dario <s265453@studenti.polito.it>
2020-10-07 11:04:56 +0200

    Change style

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,13 @@ import { Link as Scroll } from 'react-scroll';
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'left',
     alignItems: 'center',
     height: '100vh',
-    fontFamily: 'Nunito',
+    fontFamily: 'Playfair',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
   },
 
   appbar: {
@@ -25,64 +28,107 @@ const useStyles = makeStyles((theme) => ({
 
   appbarTitle: {
     flexGrow: '1',
+    color: '#000000',
   },
 
   icon: {
-    color: '#fff',
+    color: '#000000',
     fontSize: '2rem',
   },
 
   colorText: {
-    color: '#5AFF3D',
+    color: '#DC143C',
   },
 
   title: {
-    color: '#fff',
-    fontSize: '3.5rem',
+    color: '#000000',
+    fontSize: '3.7rem',
   },
 
   conteinerTitle: {
-    textAlign: 'center',
+    width: '100%',
+    textAlign: 'left',
+    margin: '5%',
   },
 
   goDown: {
-    color: '#5AFF3D',
+    color: '#DC143C',
     fontSize: '4rem',
   },
 
   title_h: {
-    color: '#fff',
+    color: '#000000',
   },
+
+  aggettivo: {
+    color: '#DC143C',
+    fontSize: '4.5rem',
+  },
+
+  containerScroll:{
+      textAlign: 'center',  
+      justifyContent: 'center',
+    alignItems: 'center',
+    },
 }));
 
 export default function Header() {
-  const aggettivi = ['Funzionale', 'Reattivo'];
+  const aggettivi = ['Funzionale', 'Reattivo', 'User Friendly'];
   const classes = useStyles();
   const [checked, setChecked] = useState(false);
   const [aggettivo, setAggettivo] = useState(0);
   const [dir, setDir] = useState(1); //crescente
   const [lunghezza, setLength] = useState(aggettivi[aggettivo].length);
   const [agg_to_display, setDisplay] = useState('');
+  const [time, setTime] = useState(true);
 
   const func_agg_to_display = useCallback(() => {
-    if (dir === 1 && lunghezza >= aggettivi[aggettivo].length) {
-      setDir(0);
-      setLength(prev => prev - 1);
-    } else if (dir === 0 && lunghezza <= 0) {
-      setDir(1);
-      if (aggettivo >= aggettivi.length - 1) {
-        setAggettivo(0);
-      } else {
-          setAggettivo(prev  => prev +1);
-      }
-      setLength(prev => prev + 1);
-    } else if (dir === 0) {
-        setLength(prev => prev - 1);
-    } else {
-        setLength(prev => prev + 1);
+    //console.log(lunghezza,aggettivo,dir)
+    if(dir === 1 && lunghezza <= aggettivi[aggettivo].length){
+        //console.log("sto aumentando e la lunghezza non è ancora quella massima")
+        if(time >= 2){
+            setLength(prev => prev + 1);
+            setTime(0)
+            setDisplay(aggettivi[aggettivo].substring(0, lunghezza) + "|");
+        } else {
+            setTime(prev => prev + 1)
+        }
+    } else if(dir === 1 && lunghezza >= aggettivi[aggettivo].length){
+        //console.log("la parola è completa")
+        /*if(time % 3){
+            console.log(aggettivi[aggettivo].substring(0, lunghezza) + " ")
+            setDisplay(aggettivi[aggettivo].substring(0, lunghezza) + ".");   
+        } else {
+            console.log(aggettivi[aggettivo].substring(0, lunghezza) + "|")
+            setDisplay(aggettivi[aggettivo].substring(0, lunghezza) + "|");   
+        }*/
+        if(time >= 5){
+            setTime(0)
+            setDir(0)
+        } else {
+            setTime(prev => prev + 1)
+        }
+    } else if (dir === 0 && lunghezza >= 0){
+        //console.log("sto diminuendo e la lunghezza non è ancora 0")
+        if(time >= 0){
+            setLength(lunghezza-1);
+            setDisplay(aggettivi[aggettivo].substring(0, lunghezza));
+            setTime(0)
+        } else {
+            setTime(prev => prev + 1)
+        }
+    } else if(dir === 0 && lunghezza < 0){
+
+        if (aggettivo >= aggettivi.length - 1) {
+            setAggettivo(0);
+          } else {
+              setAggettivo(prev  => prev +1);
+          }
+        setDir(1)
+        
     }
-    const b = aggettivi[aggettivo];
-    setDisplay(b.substring(0, lunghezza));
+
+    
   }, [dir, lunghezza, aggettivi, aggettivo, setDir, setLength, setAggettivo, setDisplay]);
 
   useEffect(() => {
@@ -92,16 +138,17 @@ export default function Header() {
   useEffect(() => {
     const myVar = setInterval(() => {
         func_agg_to_display()
-    }, 1000);
+    }, 30);
     return () => clearInterval(myVar);
-  }, [func_agg_to_display, setDisplay, aggettivi, aggettivo, lunghezza]);
+  }, [dir, lunghezza, aggettivi, aggettivo, setDir, setLength, setAggettivo, setDisplay, func_agg_to_display]);
+
 
   return (
     <div className={classes.root} id='header'>
       <AppBar className={classes.appbar} elevation={0}>
         <Toolbar className={classes.appbarWrapper}>
           <h1 className={classes.appbarTitle}>
-            my<span className={classes.colorText}>Site</span>
+            U<span className={classes.colorText}>Site</span>
           </h1>
           <IconButton>
             <SortIcon className={classes.icon} />
@@ -110,25 +157,26 @@ export default function Header() {
       </AppBar>
       <Collapse
         in={checked}
-        {...(checked ? { timeout: 2000 } : {})}
+        {...(checked ? { timeout: 3000 } : {})}
         collapsedHeight={50}>
         <div className={classes.conteinerTitle}>
-          <h1 className={classes.title}>
-            Benvenuto su <br />
-            my<span className={classes.colorText}>Site</span>
-          </h1>
           <h2 className={classes.title_h}>
-            Serve un sito{' '}
-            <span className={classes.colorText}>{agg_to_display}</span> per la
-            tua attività?
+            Benvenuto su U<span className={classes.colorText}>Site</span>
           </h2>
-          <h3 className={classes.title_h}>Sei nel posto giusto!</h3>
+          <h1 className={classes.title}>
+            Vuoi elevare la tua attività <br /> con un sito <span className={classes.aggettivo}> {agg_to_display}</span>
+          </h1>
+          <h2 className={classes.colorText} >Sei nel posto giusto!</h2>
+          
+        </div>
+
+        <div className={classes.containerScroll}> 
           <Scroll to='examples' smooth='true'>
             <IconButton>
               <ExpandMoreIcon className={classes.goDown} />
             </IconButton>
           </Scroll>
-        </div>
+          </div>
       </Collapse>
     </div>
   );
